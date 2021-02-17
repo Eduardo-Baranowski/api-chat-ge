96e754431a05c83959719c3ea9ec77a344a2e2b0
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2021-02-17 16:44:59 -0300

    mudança wsgi1

--- app.py
+++ app.py
@@ -111,6 +111,6 @@ def msg_recv():
     return(response.text)
 
 
-#if __name__ == "__main__":
-    #port = int(os.environ.get("PORT", 5000))
-    #app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
